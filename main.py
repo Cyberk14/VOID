769615947c4797674f7f853d00af3b24c051838a
Cyberk14 <139704497+Cyberk14@users.noreply.github.com>
2024-04-22 23:00:49 +0300

    Update main.py as the second input on 22-Apr

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print("this is a gti repo in repl.it")
+
+userIn = input("input anything....")
+
+if type(userIn) == int:
+	print("you entered an Integer!!!!")
+
+else:
+	print("You didi not enter an integer")
