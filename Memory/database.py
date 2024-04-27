4de8bcc6bd78728849107e636b9ca70cf90e67ef
Cyberk14 <139704497+Cyberk14@users.noreply.github.com>
2024-04-27 04:53:34 +0000

    updated database.py

--- Memory/database.py
+++ Memory/database.py
@@ -1,1 +1,24 @@
-import sqlite3
+import sqlite3 as sql
+# import embedder
+# import raw_data 
+
+class Memory:
+    # retrives data to turn it into workable chunks.
+    def get_memory(self, raw_data):
+        self.rawData = raw_data
+
+    def storeData(self, dataBase):
+        pass
+
+class ShortermMemory(Memory):
+    def update(self):
+        pass
+
+class LongtermMemory(Memory):
+    def update(self):
+        pass
+        
+
+        
+            
+
