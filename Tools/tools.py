f644cd2cdbfaeebb8430eede0b36443e47879d25
Ssengendo Nasser <ssengendonasser14@gmail.com>
2024-05-25 13:17:09 +0300

    commited on 25/5/24

--- Tools/tools.py
+++ Tools/tools.py
@@ -1,7 +1,12 @@
 tool_store = {}
 
-def tool(func):
-    def wrapper():
+def tool(func: object) -> complex:
+    def wrapper() -> complex:
         name, desc = func()
-        tool_store['name']={"func_name": func.__name__,
+        tool_store[name]={"func_name": func.__name__,
                             "func_desc": desc}
+        print(name, desc)
+        
+    return wrapper
+
+
